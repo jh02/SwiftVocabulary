b1edee933ce806d621aaec698cf1f1f4280e2001
Janee Hernandez <jlplunkett4@gmail.com>
2021-03-05 14:28:46 -0800

    Started Swift Vocabulary project.

---
+++ Swift Vocabulary/DefinitionViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DefinitionViewController.swift
+//  Swift Vocabulary
+//
+//  Created by Janee Plunkett on 3/5/21.
+//  Copyright © 2021 Janee Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var wordLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,15 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Janee Plunkett on 3/5/21.
+//  Copyright © 2021 Janee Hernandez. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    var word: String
+    var definition: String
+}
+
